9fcf1790033dad27102e87499f6b6eed27338c8e
Azure Pipelines <info@benkner-it.de>
2022-03-30 20:27:15 +0000

    Updates

---
+++ _next/static/Lnvc1QzmfBjPLNGFQ6O9n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d0ba5bade9c4dc70.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],"/about":["static/chunks/pages/about-871feafad03736e3.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
